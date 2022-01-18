957b3a67a27957c4a704dbcdb74c0b7ca41dfc0d
Padmini Pyapali <padminipyapali@Padminis-MacBook-Pro-2.local>
2022-01-18 13:03:59 -0500

    Small changes.

--- packages/hardhat-ts/hardhat.config.ts
+++ packages/hardhat-ts/hardhat.config.ts
@@ -71,31 +71,31 @@ const config: HardhatUserConfig = {
       // },
     },
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://rinkeby.infura.io/v3/7d7e7fedbfb04c27b55ef19358814bf2",
       accounts: {
         mnemonic: getMnemonic(),
       },
     },
     kovan: {
-      url: "https://kovan.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://kovan.infura.io/v3/7d7e7fedbfb04c27b55ef19358814bf2",
       accounts: {
         mnemonic: getMnemonic(),
       },
     },
     mainnet: {
-      url: "https://mainnet.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://mainnet.infura.io/v3/7d7e7fedbfb04c27b55ef19358814bf2",
       accounts: {
         mnemonic: getMnemonic(),
       },
     },
     ropsten: {
-      url: "https://ropsten.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://ropsten.infura.io/v3/7d7e7fedbfb04c27b55ef19358814bf2",
       accounts: {
         mnemonic: getMnemonic(),
       },
     },
     goerli: {
-      url: "https://goerli.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://goerli.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad",
       accounts: {
         mnemonic: getMnemonic(),
       },
--- packages/vite-app-ts/src/config/apiKeysConfig.ts
+++ packages/vite-app-ts/src/config/apiKeysConfig.ts
@@ -4,10 +4,10 @@
 // API key overview:  https://docs.ethers.io/v5/api-keys/
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
 
-export const INFURA_ID = '3ccea23c26224daf802cf31e4699d15e';
+export const INFURA_ID = "7d7e7fedbfb04c27b55ef19358814bf2";
+
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
+export const ETHERSCAN_KEY = "KV73BUW93PISVT6VIE5EVDJ7W18AJSWBJ9";
 
-export const ETHERSCAN_KEY = 'PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8';
 // BLOCKNATIVE ID FOR Notify.js:
-
-export const BLOCKNATIVE_DAPPID = '0b58206a-f3c0-4701-a62f-73c7243e8c77';
+export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
